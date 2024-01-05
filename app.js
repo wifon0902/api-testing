c595eb5373ee420f2a64b8b2d52bb177e6382303
wifon <kaziorigor0902@gmail.com>
2024-01-05 17:07:14 +0100

    tests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import API_KEY from './apikey.js';
+import { config } from './config.js';
 
+const API_KEY = config.API_KEY;
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Radomsko&units=metric';
 
 async function checkWeather (){
