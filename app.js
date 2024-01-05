3a0fd85c825e8d42c55066ec4c1b6907fb82b459
wifon <kaziorigor0902@gmail.com>
2024-01-05 18:37:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 import { config } from './config.js';
 
 const API_KEY = config.API_KEY;
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Radomsko&units=metric&appid=${API_KEY}`;
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=California&units=metric&appid=${API_KEY}`;
 
-let weather;
+let city;
 
 fetch(API_URL)
   .then((res) => res.json())
-  .then((weatherRaw) => console.log(weatherRaw.name));
+  .then((weatherRaw) => {
+    city = {
+      name: weatherRaw.name,
+      country: weatherRaw.sys.country,
+      temp: weatherRaw.main.temp,
+      humidity: weatherRaw.main.humidity,
+      sunrise: weatherRaw.sys.sunrise,
+      sunshine: weatherRaw.sys.sunset,
+      weather: weatherRaw.weather.main,
+      weatherDesc: weatherRaw.weather.description,
+    };
+    console.log(city.temp);
+  });
 
 // async function checkWeather (){
 //   const response = await fetch(API_URL + `&appid=${API_KEY}`);
